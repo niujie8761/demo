707206ae9d07c01fdfc0b790874e331429f79b87
niujie <2318741485@qq.com>
2017-12-07 16:47:23 +0800

    修改后台密码

--- Application/Home/Controller/MyselfController.class.php
+++ Application/Home/Controller/MyselfController.class.php
@@ -10,10 +10,36 @@
 namespace Home\Controller;
 
 
+use Home\Model\MangerModel;
+
 class MyselfController  extends BaseController
 {
+    /**
+     * 修改后台账户密码
+     */
     public function password()
     {
-        echo 213;
+        if(IS_AJAX) {
+            $currentPass = I('post.currentPass');
+            $newPass = I('post.newPass');
+            $kamPassword = $this->userInfo['kam_password'];
+            if($kamPassword != md5($currentPass)) {
+                $result = array(
+                    'status' => 0
+                );
+            }else {
+                $kamId = $this->userInfo['kam_id'];
+                $mangerM = MangerModel::getInstance('manger');
+                $condition = array('kam_id' => $kamId);
+                $data['kam_password'] = md5($newPass);
+                $mangerM->saveData($data, $condition);
+                $result = array(
+                    'status' => 1
+                );
+            }
+            $this->ajaxReturn($result);
+        }else {
+            $this->display();
+        }
     }
 }
